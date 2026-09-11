8b2ceb6fddc09a1548d9ea1bc04df351d82916bb
Luis Carlos da Silva Dias (https://github.com/silvadias) <silvadias.perfil@outlook.com>
2026-09-10 21:59:52 -0300

    feat: connect express routing layer with mvc controller architecture
    - Created src/routes/user.routes.js for decoupled request matching\n- Registered global /users route endpoint pattern inside src/server.js\n- Enabled full transactional API flow from client to mock database\n\n Template by: https://github.com/silvadias

--- standard/03-node-express-mvc/src/server.js
+++ standard/03-node-express-mvc/src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const env = require('./config/env');
+const userRoutes = require('./routes/user.routes');
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/users', userRoutes);
 
 app.get('/', (req, res) => {
   return res.status(200).json({
