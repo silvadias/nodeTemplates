6891903b126eb17dca9c68d2aebd222cf56bf0a8
Luis Carlos da Silva Dias (https://github.com/silvadias) <silvadias.perfil@outlook.com>
2026-09-10 23:03:46 -0300

    feat: initialize node-express-sequelize-mysql template directory
    - Scaffolded phase 04 directory using the stable MVC architecture blueprint\n- Cleared legacy node_modules to isolate upcoming database driver builds\n\n Template by: https://github.com/silvadias

---
+++ standard/04-node-express-sequelize-mysql/src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const env = require('./config/env');
+const userRoutes = require('./routes/user.routes');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/users', userRoutes);
+
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    message: "Node.js Standard Template with Express running perfectly inside Docker!",
+    status: "online",
+    environment: env.nodeEnv
+  });
+});
+
+app.listen(env.port, () => {
+  console.log(`Express server running on port ${env.port} in ${env.nodeEnv} mode`);
+});
